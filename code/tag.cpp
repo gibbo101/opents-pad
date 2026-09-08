4df229d186e06a087da23fd011fdd75e10c0f185
gibbo101 <89612238+gibbo101@users.noreply.github.com>
2026-09-08 21:15:37 +0100

    Test the tag's trigger against nullptr

--- code/tag.cpp
+++ code/tag.cpp
@@ -348,7 +348,7 @@ void TagClass::Delete_All(void)
 void TagClass::Timer_Global_Reset(int global)
 {
 	// A tag outlives a destroyed trigger with no link to take its place.
-	if (Trigger != NULL) {
+	if (Trigger != nullptr) {
 		Trigger->Reset_Global_Linked_Timed_Events(global);
 	}
 }
@@ -360,7 +360,7 @@ void TagClass::Timer_Global_Reset(int global)
 /// <param name="local">The local variable that just changed.</param>
 void TagClass::Timer_Local_Reset(int local)
 {
-	if (Trigger != NULL) {
+	if (Trigger != nullptr) {
 		Trigger->Reset_Local_Linked_Timed_Events(local);
 	}
 }
